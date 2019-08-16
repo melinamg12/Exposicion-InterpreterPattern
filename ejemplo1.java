76983f787f1fc224e8c8a8f89b2a807aeabf6807
melinamg12 <memunozga@unal.edu.co>
2019-08-15 22:06:10 -0500

    Update ejemplo1.java

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,13 +1,7 @@
-// Expression interface used to
-// check the interpreter.
 interface Expression{
     boolean interpreter(String con);
 }
   
-// TerminalExpression class implementing
-// the above interface. This interpreter 
-// just check if the data is same as the 
-// interpreter data.
 class TerminalExpression implements Expression {
     String data;
   
@@ -24,10 +18,7 @@ class TerminalExpression implements Expression {
         }
     }
 }
-// OrExpression class implementing
-// the above interface. This interpreter 
-// just returns the or condition of the 
-// data is same as the interpreter data.
+
 class OrExpression implements Expression 
 {
     Expression expr1;
@@ -41,11 +32,7 @@ class OrExpression implements Expression
         return expr1.interpreter(con) || expr2.interpreter(con);
     }
 }
-  
-// AndExpression class implementing
-// the above interface. This interpreter 
-// just returns the And condition of the 
-// data is same as the interpreter data.
+
 class AndExpression implements Expression {
     Expression expr1;
     Expression expr2;
@@ -63,20 +50,20 @@ class AndExpression implements Expression {
 class InterpreterPattern{
   
     public static void main(String[] args) {
-        Expression person1 = new TerminalExpression("Kushagra");
-        Expression person2 = new TerminalExpression("Lokesh");
+        Expression person1 = new TerminalExpression("Maria");
+        Expression person2 = new TerminalExpression("Lucas");
         Expression isSingle = new OrExpression(person1, person2);
           
-        Expression vikram = new TerminalExpression("Vikram");
-        Expression committed = new TerminalExpression("Committed");
-        Expression isCommitted = new AndExpression(vikram, committed); 	
+        Expression victor = new TerminalExpression("Victor");
+        Expression comprometido = new TerminalExpression("Comprometido");
+        Expression isCommitted = new AndExpression(victor, comprometido); 	
   
-        System.out.println(isSingle.interpreter("Kushagra"));
-        System.out.println(isSingle.interpreter("Lokesh"));
-        System.out.println(isSingle.interpreter("Achint"));
+        System.out.println(isSingle.interpreter("Maria"));
+        System.out.println(isSingle.interpreter("Lucas"));
+        System.out.println(isSingle.interpreter("Alejandro"));
           
-        System.out.println(isCommitted.interpreter("Committed, Vikram"));
-        System.out.println(isCommitted.interpreter("Single, Vikram"));
+        System.out.println(isCommitted.interpreter("Comprometido, Victor"));
+        System.out.println(isCommitted.interpreter("Soltero, Victor"));
   
     }
 }
